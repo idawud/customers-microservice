178f643f2b4ce8a8c80bf4c7788f9c88dc56c4bd
idawud <ismaildawud96@gmail.com>
2019-11-21 13:45:41 +0000

    heroku db setup

--- src/main/java/io/turntabl/springwebservice/SpringWebServiceApplication.java
+++ src/main/java/io/turntabl/springwebservice/SpringWebServiceApplication.java
@@ -4,10 +4,7 @@ import io.turntabl.springwebservice.controllers.CustomerDAO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import javax.sql.DataSource;
 
 @EnableSwagger2
 @SpringBootApplication
--- src/main/java/io/turntabl/springwebservice/configurations/DatabaseConfig.java
+++ src/main/java/io/turntabl/springwebservice/configurations/DatabaseConfig.java
@@ -2,6 +2,7 @@ package io.turntabl.springwebservice.configurations;
 
 import com.zaxxer.hikari.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.*;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -11,12 +12,14 @@ import javax.sql.DataSource;
 @Configuration
 public class DatabaseConfig {
 
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
-
     @Bean
-    public DataSource dataSource() {
+    @Profile("heroku")
+    public DataSource dataSource(
+            @Value("${spring.datasource.driver-class-name}") final String driverClass,
+            @Value("${spring.datasource.url}") final String jdbcUrl,
+            @Value("${spring.datasource.username}") final String username,
+            @Value("${spring.datasource.password}") final String password
+    ) {
         String initQuery = "" +
                 "CREATE TABLE IF NOT EXISTS customers(\n" +
                 "   id serial PRIMARY KEY,\n" +
@@ -26,8 +29,16 @@ public class DatabaseConfig {
                 "   email VARCHAR (355) UNIQUE NOT NULL,\n" +
                 "   active BOOL DEFAULT 't'\n" +
                 ");";
+
+        DataSource dataSource = DataSourceBuilder
+                                    .create()
+                                    .username(username)
+                                    .password(password)
+                                    .url(jdbcUrl)
+                                    .driverClassName(driverClass)
+                                    .build();
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
+        config.setDataSource(dataSource);
         config.setConnectionInitSql(initQuery);
         return new HikariDataSource(config);
     }

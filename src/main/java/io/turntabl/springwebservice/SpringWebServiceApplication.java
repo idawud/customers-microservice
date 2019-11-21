7cd0d930a324184e24fad92e21151a8b6c0de408
idawud <ismaildawud96@gmail.com>
2019-11-21 06:06:17 +0000

    added db connect bean

--- src/main/java/io/turntabl/springwebservice/SpringWebServiceApplication.java
+++ src/main/java/io/turntabl/springwebservice/SpringWebServiceApplication.java
@@ -3,8 +3,10 @@ package io.turntabl.springwebservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.sql.DataSource;
 
 @EnableSwagger2
 @SpringBootApplication
